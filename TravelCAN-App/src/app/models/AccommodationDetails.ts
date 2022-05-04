e767ba786ce3dddea04eb5917955dfd4d15c28b6
Arathy Unni <74705549+arathy-1993@users.noreply.github.com>
2022-05-03 21:41:46 -0400

    Update AccommodationDetails.ts

--- TravelCAN-App/src/app/models/AccommodationDetails.ts
+++ TravelCAN-App/src/app/models/AccommodationDetails.ts
@@ -1,1 +1,16 @@
-
+export class AccommodationDetails{
+    city : String;
+    rentalCategory : String;
+    highestPrice : String;
+    lowestPrice : String;
+    bedrooms : String;
+    bathrooms : String;
+    utilities : String;
+    parking : String;
+    furnished : String;
+    appliances :  String;
+    petFriendly : String;
+    address : String;
+    landlordName : String;
+    landlordPhone : String
+}
